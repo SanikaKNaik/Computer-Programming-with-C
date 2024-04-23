35e22931327cd79b33ea0ed50e8e2ad2d45ce99e
SanikaKNaik <165466738+SanikaKNaik@users.noreply.github.com>
2024-04-23 23:52:21 +0530

    Add files via upload

---
+++ Expt1/exp1_1.c.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+
+{
+	float cen, fah;
+
+	
+	printf("enter the value in degree centigrade");
+	scanf("%f", &cen);
+
+    fah=(1.8*cen)+32;
+    
+	printf("the value of temperature in degree fahrenheit is %f",fah);
+	
+}
---
+++ Expt1/exp1_2.c.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+
+{
+    float rad, area, crm;
+	
+	printf("enter the value of radius");
+	scanf("%f", &rad);
+
+	crm= 2*3.14*rad;
+	area= 3.14*rad*rad;
+	
+	printf("the circumference is %f",crm);
+	printf("the area is %f",area);
+	
+}
