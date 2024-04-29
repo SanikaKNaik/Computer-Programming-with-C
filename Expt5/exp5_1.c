3f73ef6b9f3b09db10250b4854ec9e449e5fd8e0
SanikaKNaik <165466738+SanikaKNaik@users.noreply.github.com>
2024-04-29 19:26:09 +0530

    Update exp5_1.c

--- Expt5/exp5_1.c
+++ Expt5/exp5_1.c
@@ -12,74 +12,73 @@ void main()
 	scanf("%d%d", &r2, &c2);
 	
 	if((r1==r2) && (c1==c2))
- {
-	printf("enter the elements in matrics a\n");
+       {
+	 printf("enter the elements in matrics a\n");
 	
-	for(i=1; i<=r1; i++)
-	{
+	 for(i=1; i<=r1; i++)
+	 {
 		for(j=1; j<=c1; j++)
 		{
 		
 			scanf("%d", &a[i][j]);
 		}
-	}
+	 }
+	 
+	 printf("enter the elements in matrics b\n");
 	
-	printf("enter the elements in matrics b\n");
-	
-	for(i=1; i<=r2; i++)
-	{
+	 for(i=1; i<=r2; i++)
+	 {
 		for(j=1; j<=c2; j++)
 		{
 		
 			scanf("%d",&b[i][j]);
 	
 		}
-	}	
+	 }	
 	
-
-	for(i=1; i<=r1; i++)
-	{
+         for(i=1; i<=r1; i++)
+	 {
 		for(j=1; j<=c2; j++)
 		{
 			c[i][j]=a[i][j]+b[i][j];
 		
 		}
-	}
+	 }
 	
-	printf("the  matrics a is\n");
-	for(i=1; i<=r1; i++)
-	{
+	 printf("the  matrics a is\n");
+	 for(i=1; i<=r1; i++)
+	 {
 		for(j=1; j<=c1; j++)
 		{
 		   printf("%d\t",a[i][j]);
 		}
 		printf("\n");
-	}
+	 }
 
-    printf("the  matrics b is\n");
-	for(i=1; i<=r2; i++)
-	{
+         printf("the  matrics b is\n");
+	 for(i=1; i<=r2; i++)
+	 {
 		for(j=1; j<=c2; j++)
 		{
 		   printf("%d\t",b[i][j]);
 		}
 		printf("\n");
-	}
+	 }
 	
-	printf("the sum of matrics is\n");
-	for(i=1; i<=r1; i++)
-	{
+	 printf("the sum of matrics is\n");
+	 for(i=1; i<=r1; i++)
+	 {
 		for(j=1; j<=c2; j++)
 		{
 		   printf("%d\t",c[i][j]);
 		}
 		printf("\n");
-	}
- }
- else
- {
+	 }
+       }
+      else
+      {
  	printf("error");
- }
+      }
 }
 
 
